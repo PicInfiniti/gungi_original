e7b192399f9ed0f31033c1ce680542cee5e69710
Siavash <siavash.m3070@gmail.com>
2022-08-09 22:03:31 +0430

    add hall chat

--- src/hall/main.js
+++ src/hall/main.js
@@ -1,6 +1,8 @@
 // libraries +++++++++++++++++++++++++++++++++++++
 import $ from "jquery"
-
+import {
+  socket
+} from "./assets/js/api"
 // libraries -------------------------------------
 
 // style and util +++++++++++++++++++++++++
@@ -11,8 +13,80 @@ $(document).bind("contextmenu", function (_) {
   return false;
 });
 
-$('.massage input').keypress(function(event) {
-  if(event.which == 13) {
-    console.log(`${$('.status').text()}: ${event.target.value}`);
+$('.massage input').keypress(function (event) {
+  if (event.which == 13 && event.target.value.length > 0 && $('.status h3').text()!='') {
+    $('.chat ul').append(`
+    <li>
+      <span name="player">${$('.status h3').text()}: </span>
+      <span>
+        ${event.target.value}
+      </span>
+    </li>
+    `)
+
+    $(".chat").animate({
+      scrollTop: $(".chat").height()
+    }, 300);
+
+    $('.massage input').val('')
+
+
   }
+
 });
+
+
+// $('.g-signin2').on('data-onsuccess', function (googleUser) {
+//   // Useful data for your client-side scripts:
+//   var profile = googleUser.getBasicProfile();
+
+//   profile = {
+//     id: profile.getId(),
+//     fullName: profile.getName(),
+//     givenName: profile.getGivenName(),
+//     familyName: profile.getFamilyName(),
+//     imageUrl: profile.getImageUrl(),
+//     email: profile.getEmail(),
+//   }
+
+//   socket.login(profile)
+// });
+
+
+$('.g-signin2').click(function (googleUser) {
+  // Useful data for your client-side scripts:
+  var profile = new fakeAccount();
+
+  profile = {
+    id: profile.getId,
+    fullName: profile.getName,
+    givenName: profile.getGivenName,
+    familyName: profile.getFamilyName,
+    imageUrl: profile.getImageUrl,
+    email: profile.getEmail,
+  }
+
+  socket.login(profile)
+});
+
+export class fakeAccount {
+  constructor() {
+    this.getId = makeid(21)
+    this.getName = 'Siavash Moghadas'
+    this.getGivenName = makeid(6)
+    this.getFamilyName = 'Moghadas'
+    this.getImageUrl = 'https://lh3.googleusercontent.com/a-/AFdZucqPnBxSS9ppBtdCZu4c-CFHRiOEuAhMpaqGKgLJ=s96-c'
+    this.getEmail = `${makeid(17)}@gmail.com`
+  }
+}
+
+function makeid(length) {
+  var result = '';
+  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() *
+      charactersLength));
+  }
+  return result;
+}
