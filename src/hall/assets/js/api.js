be90d3edc78b5e4b415d464e6c534ba40a1d8332
Siavash <siavash.m3070@gmail.com>
2022-08-25 20:09:21 +0430

    peer js

--- src/hall/assets/js/api.js
+++ src/hall/assets/js/api.js
@@ -1,7 +1,12 @@
 // libraries +++++++++++++++++++++++++++++++++++++
 import $ from "jquery"
 export var socket = {}
-var BaseUrl = 'http://192.168.1.11:5000'
+var BaseUrl = 'http://gungi.pythonanywhere.com/'
+
+socket.users = []
+socket.main = null
+socket.activeUser = null
+
 
 socket.login = function (json) {
   $.ajax({
@@ -17,16 +22,16 @@ socket.login = function (json) {
         <img src=${json['imageUrl']} alt="">
         <h3>${json['givenName']}</h3>
       `)
-      for(let player in data['players']){
-        if(player!=json['id']){
-          $('.players').append(      `
-          <div id=${player}>
-            <img src=${data['players'][player]['imageUrl']} alt="">
-            <h3>${data['players'][player]['givenName']}</h3>
-          </div>
-        `)
-        }
-      }
+      // for(let player in data['players']){
+      //   if(player!=json['id']){
+      //     $('.players').append(      `
+      //     <div id=${player}>
+      //       <img src=${data['players'][player]['imageUrl']} alt="">
+      //       <h3>${data['players'][player]['givenName']}</h3>
+      //     </div>
+      //   `)
+      //   }
+      // }
 
       for(let massage of data['chatList']){
         $('.chat ul').append(`
@@ -45,16 +50,29 @@ socket.login = function (json) {
   });
 }
 
-socket.players = function (json) {
+socket.players = function () {
   $.ajax({
     type: "GET",
     url: `${BaseUrl}/players`,
     // The key needs to match your method's input parameter (case-sensitive).
-    data: JSON.stringify(json),
     contentType: "application/json; charset=utf-8",
     dataType: "json",
     success: function (data) {
-      $('.g-signin2').remove()
+      for(let player in data['players']){
+        if(!socket.users.includes(player) && (!socket.main || socket.main.id!=player)){
+          $('.players').append(      `
+          <div id=${player}  desc="player">
+            <img src=${data['players'][player]['imageUrl']} alt="">
+            <h3>${data['players'][player]['givenName']}</h3>
+          </div>
+        `)
+        socket.users.push(player)
+        $(`#${player}`).click((event)=>{
+          socket.activeUser = data['players'][event.delegateTarget.getAttribute('id')]['idPeer']
+          console.log(socket.activeUser)
+        })
+        }
+      }
     },
     error: function (errMsg) {
       console.log(errMsg);
@@ -78,3 +96,4 @@ socket.chat = function (json) {
     }
   });
 }
+
--- src/hall/main.js
+++ src/hall/main.js
@@ -1,5 +1,7 @@
 // libraries +++++++++++++++++++++++++++++++++++++
 import $ from "jquery"
+
+import './assets/js/peer'
 import {
   socket
 } from "./assets/js/api"
@@ -13,30 +15,24 @@ $(document).bind("contextmenu", function (_) {
   return false;
 });
 
-$('.massage input').keypress(function (event) {
-  
-  if (event.which == 13 && event.target.value.length > 0 && $('.status h3').text()!='') {
-    socket.chat({name: $('.status h3').text(), massage: event.target.value})
-    $('.chat ul').append(`
-    <li>
-      <span name="player">${$('.status h3').text()}: </span>
-      <span>
-        ${event.target.value}
-      </span>
-    </li>
-    `)
 
-    $(".chat").animate({
-      scrollTop: $(".chat").height()
-    }, 300);
+setInterval(socket.players, 2000)
 
-    $('.massage input').val('')
 
-    
+
+
+
+
+export class fakeAccount {
+  constructor() {
+    this.getId = makeid(21)
+    this.getName = 'Siavash Moghadas'
+    this.getGivenName = makeid(6)
+    this.getFamilyName = 'Moghadas'
+    this.getImageUrl = 'https://lh3.googleusercontent.com/a-/AFdZucqPnBxSS9ppBtdCZu4c-CFHRiOEuAhMpaqGKgLJ=s96-c'
+    this.getEmail = `${makeid(17)}@gmail.com`
   }
-
-});
-
+}
 
 // $('.g-signin2').on('data-onsuccess', function (googleUser) {
 //   // Useful data for your client-side scripts:
@@ -54,33 +50,42 @@ $('.massage input').keypress(function (event) {
 //   socket.login(profile)
 // });
 
-
-$('.g-signin2').click(function (googleUser) {
-  // Useful data for your client-side scripts:
-  var profile = new fakeAccount();
-
-  profile = {
-    id: profile.getId,
-    fullName: profile.getName,
-    givenName: profile.getGivenName,
-    familyName: profile.getFamilyName,
-    imageUrl: profile.getImageUrl,
-    email: profile.getEmail,
-  }
-
-  socket.login(profile)
+var peer = new Peer();
+peer.on('open', function(id) {
+  profile['idPeer'] = id
+  console.log(id)
 });
 
-export class fakeAccount {
-  constructor() {
-    this.getId = makeid(21)
-    this.getName = 'Siavash Moghadas'
-    this.getGivenName = makeid(6)
-    this.getFamilyName = 'Moghadas'
-    this.getImageUrl = 'https://lh3.googleusercontent.com/a-/AFdZucqPnBxSS9ppBtdCZu4c-CFHRiOEuAhMpaqGKgLJ=s96-c'
-    this.getEmail = `${makeid(17)}@gmail.com`
-  }
+peer.on('connection', function(conn) {
+  conn.on('data', function(data){
+    console.log(data)
+    $('.chat ul').append(`
+    <li>
+      <span name="player">${$('.status h3').text()}: </span>
+      <span>
+        ${data}
+      </span>
+    </li>
+    `)
+  });
+});
+
+var profile = new fakeAccount();
+profile = {
+  id: profile.getId,
+  fullName: profile.getName,
+  givenName: profile.getGivenName,
+  familyName: profile.getFamilyName,
+  imageUrl: profile.getImageUrl,
+  email: profile.getEmail,
 }
+
+
+
+$('.g-signin2').click(function (googleUser) {
+  socket.main = profile
+  socket.login(profile)
+});
 
 function makeid(length) {
   var result = '';
@@ -92,3 +97,40 @@ function makeid(length) {
   }
   return result;
 }
+
+
+// var conn = peer.connect('another-peers-id');
+// // on open will be launch when you successfully connect to PeerServer
+// conn.on('open', function(){
+//   // here you have conn.id
+//   conn.send('hi!');
+// });
+
+
+$('.massage input').keypress(function (event) {
+  
+  if (event.which == 13 && event.target.value.length > 0 && $('.status h3').text() != '') {
+    let conn = peer.connect(socket.activeUser);
+
+    $('.chat ul').append(`
+    <li>
+      <span name="player">${$('.status h3').text()}: </span>
+      <span>
+        ${event.target.value}
+      </span>
+    </li>
+    `)
+
+    $(".chat").animate({
+      scrollTop: $(".chat").height()
+    }, 300);
+    // on open will be launch when you successfully connect to PeerServer
+
+    conn.on('open', function(){
+      conn.send(event.target.value);
+      $('.massage input').val('')
+    });
+  }
+});
+
+
